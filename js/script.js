0d049717ca21de183543efcaa146b40b95888dbd
Ivan <popenya4554@gmail.com>
2019-11-18 22:21:48 +0300

    Запущены с начала

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var lebl = 5;
+const P = 3.14;
+let seven = 7;
+
+console.log (lebl);
+console.log (seven);
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(number * 10);
+
+let persone = {
+    name: "Jonh",
+    age: 20,
+    isMarried: false
+};
+
+console.log (persone["name"]);
+
+alert ("Ваня красавчик");
+// let answer = confirm("Вам есть 18 лет?");
+
+let answer = prompt("Вам есть 18 лет?", "да");
+
+console.log(answer);
